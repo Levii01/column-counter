eb7fcfb65d95bb57be11c43fdb94e30724a40432
Levii01 <m.sedziak01@gmail.com>
2017-11-14 20:01:15 +0100

    Check [-a] param

--- script.sh
+++ script.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+function error_with {
+  echo "$1"
+  exit 1
+}
+
+function script_name {
+  basename "$0"
+}
+
 function syntax_error {
   echo "Błąd składni"
 }
@@ -11,6 +20,34 @@ function variable_error {
 function access_error {
   echo "Brak odpowiednich praw dostępu"
 }
+
+
+# Start
+if [[ "$#" = 0 ]]; then
+  error_with 'Nie podałeś żadnej zmiennej do skryptu \nspróbuj `script.sh  plik.txt  3 5`'
+fi
+
+if [[ "$1" = '-a' ]]; then
+  sum_result=true
+  shift
+fi
+
+if [ $sum_result ] ; then
+  echo "z parametrem -a"
+fi
+
+echo "Uruchomiłeś program `basename $0`"
+echo Wszystkie parametry: $@
+
+# przypisanie pliku
+file=$1
+echo Plik z którego odczytam wartości: $file
+shift
+
+columns=$@
+echo Kolumny które zsumuje: ${columns}
+
+# script_name
 # syntax_error
 # variable_error
 # access_error
