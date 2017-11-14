ead7d38a82c97af9172b2ac36b7fa568dc5cbacc
Levii01 <m.sedziak01@gmail.com>
2017-11-14 23:42:21 +0100

    Format refactor

--- script.sh
+++ script.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 function general_error_with {
   echo $1
   echo "Przykład: script.sh [-a] plik.txt [kolumna1 kolumna2]"
@@ -6,7 +7,7 @@ function general_error_with {
 }
 
 function build_error_with {
-  echo $@
+  echo $1
   exit 2
 }
 
@@ -35,31 +36,31 @@ if [ ! -f $file ]; then
 fi
 
 if [ ! -r $file ]; then
-	build_error_with "Brak odpowiednich praw dostępu do pliku"
+  build_error_with "Brak odpowiednich praw dostępu do pliku"
 fi
 
 if [ $# -eq 0 ]; then
-	if [ $sum_result_flag ] ; then
-  awk '{for(i=1;i<=NF;i++){sum[i]+=$i}} END {for(i=1;i<=NF;i++){print wyn+=sum[i]} print wyn}' $file
-  # sumowanie kazdej liczby w kolumnie, kolejna petla tworzy wynikowa kolumne liczb i ja wypisuje, sumuje i wyswietla jej wartosc.
-	exit 0
-	fi
+  if [ $sum_result_flag ] ; then
+    awk '{for(i=1;i<=NF;i++){sum[i]+=$i}} END {for(i=1;i<=NF;i++){print wyn+=sum[i]} print wyn}' $file
+    # sumowanie kazdej liczby w kolumnie, kolejna petla tworzy wynikowa kolumne liczb i ja wypisuje, sumuje i wyswietla jej wartosc.
+    exit 0
+  fi
 
-awk '{for(i=1;i<=NF;i++){sum[i]+=$i}} END {for(i=1;i<=NF;i++){print sum[i]}}' $file
-exit 0
+  awk '{for(i=1;i<=NF;i++){sum[i]+=$i}} END {for(i=1;i<=NF;i++){print sum[i]}}' $file
+  exit 0
 fi
 
 while [ $# -gt 0 ]; do
-	sum_column=`awk -v x=$1 '{y+=$x} END {print y}' $file`
+  sum_column=`awk -v x=$1 '{y+=$x} END {print y}' $file`
   # sumowanie kolumny do jednej zmiennej
-	echo $sum_column
+  echo $sum_column
 
-	if [ $sum_result_flag ]; then
-		let result_columns+=$sum_column
-	fi
-	shift
+  if [ $sum_result_flag ]; then
+  	let result_columns+=$sum_column
+  fi
+  shift
 done
 
 if [ $sum_result_flag ]; then
-	echo $result_columns
+  echo $result_columns
 fi
