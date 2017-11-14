268df758f66f5f8bff98a349f2986615acfda58c
Levii01 <m.sedziak01@gmail.com>
2017-11-14 23:36:15 +0100

    Write loop with awk command

--- script.sh
+++ script.sh
@@ -1,53 +1,65 @@
 #!/usr/bin/env bash
-
-function error_with {
-  echo "$1"
+function general_error_with {
+  echo $1
+  echo "Przykład: script.sh [-a] plik.txt [kolumna1 kolumna2]"
   exit 1
 }
 
-function script_name {
-  basename "$0"
+function build_error_with {
+  echo $@
+  exit 2
 }
 
-function syntax_error {
-  echo "Błąd składni"
-}
-
-function variable_error {
-  echo "Użycie niepoprawnego argumentu"
-}
-
-function access_error {
-  echo "Brak odpowiednich praw dostępu"
-}
-
-
-# Start
 if [[ "$#" = 0 ]]; then
-  error_with 'Nie podałeś żadnej zmiennej do skryptu \nspróbuj `script.sh  plik.txt  3 5`'
+  general_error_with "Nie podałeś żadnego argumentu"
 fi
 
-if [[ "$1" = '-a' ]]; then
-  sum_result=true
+if [[ "$1" =~ "-" ]]; then
+  if [[ "$1" = '-a' ]]; then
+    sum_result_flag=true
+    shift
+  else
+    general_error_with "Zła opcja, popraw składnię"
+  fi
+fi
+
+if [[ "$#" = 0 ]]; then
+  general_error_with "Za mało argumentów, podaj nazwę pliku"
+else
+  file=$1
   shift
 fi
 
-if [ $sum_result ] ; then
-  echo "z parametrem -a"
+if [ ! -f $file ]; then
+  build_error_with "Podany plik nie istnieje"
 fi
 
-echo "Uruchomiłeś program `basename $0`"
-echo Wszystkie parametry: $@
+if [ ! -r $file ]; then
+	build_error_with "Brak odpowiednich praw dostępu do pliku"
+fi
 
-# przypisanie pliku
-file=$1
-echo Plik z którego odczytam wartości: $file
-shift
+if [ $# -eq 0 ]; then
+	if [ $sum_result_flag ] ; then
+  awk '{for(i=1;i<=NF;i++){sum[i]+=$i}} END {for(i=1;i<=NF;i++){print wyn+=sum[i]} print wyn}' $file
+  # sumowanie kazdej liczby w kolumnie, kolejna petla tworzy wynikowa kolumne liczb i ja wypisuje, sumuje i wyswietla jej wartosc.
+	exit 0
+	fi
 
-columns=$@
-echo Kolumny które zsumuje: ${columns}
+awk '{for(i=1;i<=NF;i++){sum[i]+=$i}} END {for(i=1;i<=NF;i++){print sum[i]}}' $file
+exit 0
+fi
 
-# script_name
-# syntax_error
-# variable_error
-# access_error
+while [ $# -gt 0 ]; do
+	sum_column=`awk -v x=$1 '{y+=$x} END {print y}' $file`
+  # sumowanie kolumny do jednej zmiennej
+	echo $sum_column
+
+	if [ $sum_result_flag ]; then
+		let result_columns+=$sum_column
+	fi
+	shift
+done
+
+if [ $sum_result_flag ]; then
+	echo $result_columns
+fi
